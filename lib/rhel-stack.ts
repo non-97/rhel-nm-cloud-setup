e93b7b248d58a2f82e47eabecf1ea36609f71c53
non-97 <nonmonaka07@gmail.com>
2023-08-19 15:28:02 +0900

    :tada: Initial commit

--- lib/rhel-stack.ts
+++ lib/rhel-stack.ts
@@ -1,16 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Vpc } from "./constructs/vpc";
+import { Ec2Instance } from "./constructs/ec2-instance";
 
 export class RhelStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new Vpc(this, "Vpc");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RhelQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Zabbix Server
+    const instanceA = new Ec2Instance(this, "Ec2InstanceA", {
+      vpc: vpc.vpc,
+    });
+
+    instanceA.instance.connections.allowFrom(
+      cdk.aws_ec2.Peer.anyIpv4(),
+      cdk.aws_ec2.Port.allIcmp()
+    );
   }
 }
